1fd2c7fbbf26e12ae1aef94b2af8190d30973168
Goodsebas <166456139+Goodsebas@users.noreply.github.com>
2024-08-13 20:33:10 -0500

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,17 @@
-HOLA MUNDO
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "XD": "Es otra forma de respuesta a algo gracioso",
+            "RUSH": "Es una forma de ir a por un enemigo en un juego",
+            "F": "Es una forma de respuesta a algo malo que paso oh una muerte",
+            "GG": "Es una forma de respuesta a algo bueno que ah pasado en un juego como una victoria"
+            }
+while True:
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!) o 'SALIR' para terminar: ")
+    
+    if word == "SALIR":
+        break
+    elif word in meme_dict:
+        print(meme_dict[word])
+    else:
+        print("Palabra no encontrada en el diccionario.")
