9db3962d21718cca2f8543a8a4c7a550958fdb09
Goodsebas <166456139+Goodsebas@users.noreply.github.com>
2024-09-03 22:04:46 -0500

    Update a.py

--- a.py
+++ a.py
@@ -31,4 +31,20 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+    
+    In reality, this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        if ctx.subcommand_passed:
+            user = ctx.message.mentions[0] if ctx.message.mentions else None
+            if user:
+                await ctx.send(f'Yes, {user.display_name} is cool.')
+            else:
+                await ctx.send(f'No, I could not find the user you mentioned.')
+        else:
+            await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
 bot.run("")
